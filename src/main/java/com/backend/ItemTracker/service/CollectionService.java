b95fd47183306a29f22baf96108ab1dc3c7aa2bd
janiek <janiek2011@hotmail.com>
2022-06-08 18:36:39 +0200

    Collection files added
    Collection files added

---
+++ src/main/java/com/backend/ItemTracker/service/CollectionService.java
@@ -0,0 +1,36 @@
+package com.backend.ItemTracker.service;
+
+import com.backend.ItemTracker.model.Collection;
+import com.backend.ItemTracker.model.Item;
+import com.backend.ItemTracker.repository.CollectionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class CollectionService {
+    private final CollectionRepository collectionRepository;
+
+    @Autowired
+    public CollectionService(CollectionRepository collectionRepository) {
+        this.collectionRepository = collectionRepository;
+    }
+
+    public Collection FindCollectionById(Long id) {
+        Collection collection = collectionRepository.findById(id).get();
+        return collection;
+    }
+
+    public Collection Create(Collection collection) {
+        Collection savedCollection = collectionRepository.save(collection);
+        return savedCollection;
+    }
+
+    public Collection Update(Collection collection) {
+        Collection savedCollection = collectionRepository.save(collection);
+        return savedCollection;
+    }
+
+    public void Delete(Long id) {
+        collectionRepository.deleteById(id);
+    }
+}
